18d7190ffea95b2177fecf90cd264dd2ce395840
MORO <unicorng183@gmail.com>
2026-08-22 12:53:18 -0700

    Create calc.java
    java code for account balance inquiries

---
+++ calc.java
@@ -0,0 +1,20 @@
+public record calc() {
+    public static void 
+
+     main(String[]  args )   {                                                       //variables decleration
+      
+        System.out.println("Please Enter your name:");
+        System.out.println("Enter your account id:\n");                            //id details
+     
+        System.out.println("Check your mobile app authunticator.\n");
+        
+         System.out.println("Welcome back : %d\n" );
+
+        System.out.println("Your Balance = %d");                                     //budget info
+
+
+     }
+
+
+    
+}
