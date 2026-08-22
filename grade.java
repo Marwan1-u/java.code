54c4a98d804b9a9d2df70ac9045818c0701bb2fe
MORO <unicorng183@gmail.com>
2026-08-22 13:34:18 -0700

    Create grade.java
    grade calculator

---
+++ grade.java
@@ -0,0 +1,26 @@
+public class grade {
+    
+   public static void 
+
+   main( String[] args) {
+     
+    int grade=92;
+    if (grade>=96) {
+        
+        System.out.printf("%d = A+ \n"  , grade);
+
+
+    }else if(grade>=90 &&grade <=92) {
+
+
+       System.out.printf("%d = A \n" , grade);
+
+    }
+
+    System.out.println("Thank you for your cooperation& understanding.\n");
+     
+    System.out.println("Contact support. \n");
+
+   }
+
+}
